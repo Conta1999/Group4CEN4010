2a2496319704a3c5857567ef63cb9235f120b34a
ZackZeta <107325334+ZackZeta@users.noreply.github.com>
2022-06-22 20:20:11 -0400

    Book details creation

---
+++ backend/src/main/java/com/bookstore/backend/BookDetails.java
@@ -0,0 +1,138 @@
+package com.bookstore.backend;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.annotation.Generated;
+//import javax.persistence.Entity;
+
+
+//@Entity
+public class BookDetails implements Serializable{
+    
+    // need to fix import javax.persistence.Entity;
+    //@ID
+    //@GeneratedValue(strategy = GenertionType.AUTO)
+    //@column(null = false, updatable = false)
+
+
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        private int id;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        private int iSBN;
+
+        public int getiSBN() {
+            return iSBN;
+        }
+
+        public void setiSBN(int iSBN) {
+            this.iSBN = iSBN;
+        }
+
+        private String publisher; 
+        public String getPublisher() {
+            return publisher;
+        }
+
+        public void setPublisher(String publisher) {
+            this.publisher = publisher;
+        }
+        private String Author;
+        public String getAuthor() {
+            return Author;
+        }
+
+        public void setAuthor(String author) {
+            Author = author;
+        }
+
+        private String Genre;
+
+        public String getGenre() {
+            return Genre;
+        }
+
+        public void setGenre(String genre) {
+            Genre = genre;
+        }
+
+       
+        private Double price;
+
+        public Double getPrice() {
+            return price;
+        }
+
+        public void setPrice(Double price) {
+            this.price = price;
+        }
+
+        private Date yearPublished;
+
+        public Date getYearPublished() {
+            return yearPublished;
+        }
+
+        public void setYearPublished(Date yearPublished) {
+            this.yearPublished = yearPublished;
+        }
+
+        private int copiesSold = 0;
+           
+        public int getCopiesSold() {
+            return copiesSold;
+        }
+
+        public void setCopiesSold(int copiesSold) {
+            this.copiesSold = copiesSold;
+        }
+
+    public BookDetails(String name, int id, int iSBN, String publisher , String Author , String Genre , Double price , Date yearPublished, int copiesSold )
+    {
+        this.name = name;
+        this.id = id;
+        this.iSBN = iSBN;
+        this.publisher = publisher;
+        this.Author = Author;
+        this.Genre = Genre;
+        this.price = price;
+        this.yearPublished = yearPublished;
+        this.copiesSold = copiesSold;
+
+    }
+
+    @Override
+    public String toString()
+    {
+        return         
+        "Book Details:" + '\'' +
+        "Book Name: " + name + '\'' +
+        "ID: " +id + '\'' +
+        "ISBN: " + iSBN + '\'' +
+        "Publisher: " + publisher + '\'' +
+        "Author: " + Author + '\'' +
+        "Genre: " + Genre + '\'' +
+        "Price: " + price + '\'' +
+        "Year Published: " + yearPublished + '\'' +
+        "Copies Sold: " + copiesSold; 
+    }
+
+    
+}
