09f6b74b5b1b7c92977710a92aefda26a376d030
Jdiaz601 <jdiaz601@fiu.edu>
2022-06-22 21:11:48 -0400

    Update BookRating.java

--- backend/src/main/java/com/bookstore/backend/BookRating.java
+++ backend/src/main/java/com/bookstore/backend/BookRating.java
@@ -1,9 +1,27 @@
 package com.bookstore.backend;
 
-public class BookRating 
-{
+import java.util.Date;
+import java.time.LocalDateTime;
+
+public class BookRating {
+    
+    private String Author;
+    private int id;
+    private String name;
+    private Date yearPublished;
+    
     
 
+    public void setId() {
+        this.id = id;
+    }
 
+    public int getID() {
+        return id;
+    }
     
+    public BookRating () {
+
+
+    }
 }
