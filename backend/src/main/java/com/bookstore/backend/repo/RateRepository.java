153afe20f5bc07e6294f67e027f81c22bad39f90
Jdiaz601 <jdiaz601@fiu.edu>
2022-07-23 19:39:05 -0400

    Update RateRepository.java

--- backend/src/main/java/com/bookstore/backend/repo/RateRepository.java
+++ backend/src/main/java/com/bookstore/backend/repo/RateRepository.java
@@ -1,5 +1,21 @@
 package com.bookstore.backend.repo;
+import com.bookstore.backend.model.BookRating;
 
-public class RateRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+import java.util.List;
+
+@Repository
+
+public interface RateRepository  extends JpaRepository<BookRating, String>{
+    @Query("{'id': ?0}")
+    Optional<BookRating> findById(String id);
+
+    @Query("{'userid': ?0}")
+    Optional<List<BookRating>> findByUserId(String userid);
+
+    @Query("{'bookid': ?0}")
+    Optional<List<BookRating>> findByBookId(String bookid);
 }
